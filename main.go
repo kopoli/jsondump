a2660b7c79f6753312f6dd04b58d1dc1b8be359a
Kalle Kankare <kalle.kankare@iki.fi>
2020-12-13 12:01:34 +0200

    main: Add boilerplate starting

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/kopoli/appkit"
+)
+
+var (
+	version     = "Undefined"
+	timestamp   = "Undefined"
+	buildGOOS   = "Undefined"
+	buildGOARCH = "Undefined"
+	progVersion = "" + version
+)
+
+func main() {
+	checkErr := func(err error) {
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed with error %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	var err error
+
+	dbpath := "testdb"
+	opts := appkit.NewOptions()
+	opts.Set("program-name", os.Args[0])
+	opts.Set("program-version", progVersion)
+	opts.Set("program-timestamp", timestamp)
+	opts.Set("program-buildgoos", buildGOOS)
+	opts.Set("program-buildgoarch", buildGOARCH)
+
+	base := NewCommand(nil, "", "")
+	base.Flags.Usage = func() {
+		out := base.Flags.Output()
+		fmt.Fprintf(out,
+			"%s: Take notes on foods\n\nCommands:\n",
+			os.Args[0])
+		base.CommandList(out)
+		if HasFlags(base.Flags) {
+			fmt.Fprintf(out, "\nOptions:\n")
+			base.Flags.PrintDefaults()
+		}
+	}
+	optVerbose := base.Flags.Bool("verbose", false, "Enable verbose output")
+	optVersion := base.Flags.Bool("version", false, "Display version")
+	optDbPath := base.Flags.String("db-path", dbpath, "Database path")
+
+	web := NewCommand(base, "start-web web", "Start web server")
+	optAddr := web.Flags.String("address", ":8042", "Listen address and port")
+	optTimestampLog := web.Flags.Bool("log-timestamps", false, "Write timestamps to log")
+
+	err = base.Parse(os.Args[1:], opts)
+	if err == flag.ErrHelp {
+		os.Exit(0)
+	}
+	checkErr(err)
+
+	if *optVerbose {
+		opts.Set("verbose", "t")
+	}
+
+	if *optVersion {
+		fmt.Println(appkit.VersionString(opts))
+		os.Exit(0)
+	}
+
+	dbpath = *optDbPath
+	err = os.MkdirAll(dbpath, 0755)
+	checkErr(err)
+
+	cmd := opts.Get("cmdline-command", "")
+	// args := SplitArguments(opts.Get("cmdline-args", ""))
+
+	switch cmd {
+	case "start-web":
+		opts.Set("address", *optAddr)
+		if *optTimestampLog {
+			opts.Set("log-timestamps", "t")
+		}
+		// err = StartWeb(opts)
+		// checkErr(err)
+		return
+	}
+}
